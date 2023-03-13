8964399225086859438698bfb400e2c3a8986e40
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-03-13 10:05:13 -0700

    My final work

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    i = len(my_list)
-    for i in my_list:
-        print("{:d}".format(i))
-        i -= 1
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
