90f09550d6874bbc042164d993b75745a6acd075
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-06-19 07:24:41 -0700

    update of select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 import MySQLdb
+import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+# Read the command-line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+#connent to Mysql server
+conn = MySQLdb.connect(user=username, passwd=password, db=database)
+
+#perform operation
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+#fetch and print result
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
+
+#close connection
 cur.close()
 conn.close()
