41f1aeaf63908ae9e1dc1339604f9f8f5736993d
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2023-06-23 05:30:38 -0700

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states starting with N from the database hbtn_0e_0_usa """
+"""  lists all cities from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
+    cur.execute("SELECT * FROM cities")
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
+
